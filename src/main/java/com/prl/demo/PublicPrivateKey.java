a4cdb8dc641d55c96ebf3728ce2a73543776debf
peirenlei <peirenlei@163.com>
2014-08-09 15:43:50 +0800

    公钥私钥,stream

---
+++ src/main/java/com/prl/demo/PublicPrivateKey.java
@@ -0,0 +1,68 @@
+package com.prl.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+public class PublicPrivateKey {
+
+	public static final String AlgorithRSA = "RSA";
+	public static String SrcString = "helloworld";
+	public static byte[] Salt = new byte[] { (byte) 0x8E, 0x12, 0x39, (byte) 0x9C, 0x07, 0x72, 0x6F, 0x5A };
+
+	public static void main(String[] args) throws Exception {
+		demo1();
+	}
+
+	public static void demo() throws Exception {
+		Cipher cipher = Cipher.getInstance(AlgorithRSA);
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(AlgorithRSA);
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		PublicKey publicKey = keyPair.getPublic();
+		PrivateKey privateKey = keyPair.getPrivate();
+
+		// 加密
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] result = cipher.doFinal(SrcString.getBytes());
+		System.out.println(new String(result));
+
+		// 解密
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		byte[] src = cipher.doFinal(result);
+		System.out.println(new String(src));
+	}
+
+	public static void demo1() throws Exception {
+		Cipher cipher = Cipher.getInstance(AlgorithRSA);
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(AlgorithRSA);
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		PublicKey publicKey = keyPair.getPublic();
+		PrivateKey privateKey = keyPair.getPrivate();
+
+		// 加密
+		ByteArrayInputStream is = new ByteArrayInputStream(SrcString.getBytes());
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		CipherInputStream cipherInputStream = new CipherInputStream(is, cipher);
+		IOUtils.copy(cipherInputStream, outputStream);
+		
+		System.out.println("after:" + new String(outputStream.toByteArray()));
+
+		// 解密
+		ByteArrayInputStream bis = new ByteArrayInputStream(outputStream.toByteArray());
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		cipherInputStream = new CipherInputStream(bis, cipher);
+		ByteArrayOutputStream outputStream1 = new ByteArrayOutputStream();
+		IOUtils.copy(cipherInputStream, outputStream1);
+		System.out.println("before:" + new String(outputStream1.toByteArray()));
+	}
+}
--- src/main/java/com/prl/demo/SecurityTest.java
+++ src/main/java/com/prl/demo/SecurityTest.java
@@ -3,26 +3,58 @@ package com.prl.demo;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-
+import java.security.spec.KeySpec;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 
+@SuppressWarnings("unused")
 public class SecurityTest {
-	
-	public static final String AlgorithName = "AES";
+	public static final String AlgorithAES = "AES";
+	public static final String AlgorithPBE = "PBEWithMD5AndDES";
+	public static String SrcString = "helloworld";
+	public static String Password = "yourpassword";
+	public static byte[] Salt = new byte[] { (byte) 0x8E, 0x12, 0x39, (byte) 0x9C, 0x07, 0x72, 0x6F, 0x5A };
 
-	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		Cipher cipher = Cipher.getInstance(AlgorithName);
-		Key securityKey = KeyGenerator.getInstance(AlgorithName).generateKey();
+	public static void main(String[] args) throws Exception {
+		test2();
+	}
+
+	private static void test1() throws Exception {
+		// 加密
+		Cipher cipher = Cipher.getInstance(AlgorithAES);
+		Key securityKey = KeyGenerator.getInstance(AlgorithAES).generateKey();
 		cipher.init(Cipher.ENCRYPT_MODE, securityKey);
-		byte[] result = cipher.doFinal("helloworld".getBytes());
+		byte[] result = cipher.doFinal(SrcString.getBytes());
 		System.out.println(new String(result));
-		
+
+		// 解密
 		cipher.init(Cipher.DECRYPT_MODE, securityKey);
 		byte[] src = cipher.doFinal(result);
 		System.out.println(new String(src));
 	}
+
+	public static void test2() throws Exception {
+		KeySpec keySpec = new PBEKeySpec(Password.toCharArray());
+		SecretKey key = SecretKeyFactory.getInstance(AlgorithPBE).generateSecret(keySpec);
+		PBEParameterSpec parameterSpec = new PBEParameterSpec(Salt, 1000);
+		Cipher cipher = Cipher.getInstance(AlgorithPBE);
+
+		// 加密
+		cipher.init(Cipher.ENCRYPT_MODE, key, parameterSpec);
+		byte[] result = cipher.doFinal(SrcString.getBytes());
+		System.out.println(new String(result));
+
+		// 解密
+		cipher.init(Cipher.DECRYPT_MODE, key, parameterSpec);
+		byte[] src = cipher.doFinal(result);
+		System.out.println(new String(src));
+	}
+
 }
